ba7be4d559421866dbff0f705fc084ed473734e7
nuri1995 <56094702+nuri1995@users.noreply.github.com>
2021-01-02 18:29:41 +0100

    prueba
    prueba

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ImagesComponent } from './components/images/images.component';
+import { ImageComponent } from './components/image/image.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [AppComponent, ImagesComponent, ImageComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
